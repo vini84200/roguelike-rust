70b49c8a3d43202ce29fd885e77867d186705d5b
vini84200 <www.vini84200@hotmail.com>
2020-11-22 14:54:28 -0300

    Movendo um personagem pela tela

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use tcod::colors::*;
+use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
+
+// actual size of the window
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+
+struct Tcod {
+    root: Root,
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, ..} => *player_y -= 1,
+        Key { code: Down, ..} => *player_y += 1,
+        Key { code: Left, ..} => *player_x -= 1,
+        Key { code: Right, ..} => *player_x += 1,
+        Key { code: Escape, ..} => {
+            return true;
+        }
+        _ => {}
+    }
+    false
+}
+
 fn main() {
-    println!("Hello, world!");
+    let root = Root::initializer()
+    .font("arial10x10.png", FontLayout::Tcod)
+    .font_type(FontType::Greyscale)
+    .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+    .title("Rust/libtcod tutorial")
+    .init();
+
+    let mut tcod = Tcod { root };
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+    }
 }
