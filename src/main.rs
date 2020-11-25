061dbce8157b3b2ab31611c3060e019a54a8356b
vini84200 <www.vini84200@hotmail.com>
2020-11-25 14:15:43 -0300

    Drawing to a offscreen and composing.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
+    con: Offscreen,
 }
 
 fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
@@ -36,7 +37,9 @@ fn main() {
     .title("Rust/libtcod tutorial")
     .init();
 
-    let mut tcod = Tcod { root };
+    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let mut tcod = Tcod { root, con };
 
     tcod::system::set_fps(LIMIT_FPS);
 
@@ -44,9 +47,20 @@ fn main() {
     let mut player_y = SCREEN_HEIGHT / 2;
 
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.con.set_default_foreground(WHITE);
+        tcod.con.clear();
+        tcod.con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+
+        blit(
+            &tcod.con,
+            (0,0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0,0),
+            1.0,
+            1.0,
+        );
+
         tcod.root.flush();
         let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
         if exit {
