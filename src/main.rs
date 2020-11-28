e480562e41628dd1951facc4865f5d871a0cd116
vini84200 <www.vini84200@hotmail.com>
2020-11-28 08:35:48 -0300

    Generating objects and blocking

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ const SCREEN_HEIGHT: i32 = 50;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
+const PLAYER: usize = 0;
+
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_LIGHT_WALL: Color = Color {
     r: 130,
@@ -38,6 +40,8 @@ const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
 const FOV_LIGHT_WALLS: bool = true;
 const TORCH_RADIUS: i32 = 10;
 
+const MAX_ROOM_MONSTERS: i32 = 3;
+
 const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
@@ -51,25 +55,48 @@ struct Object {
     x: i32,
     y: i32,
     char: char,
-    color: Color
+    color: Color,
+    name: String,
+    blocks: bool,
+    alive: bool,
 }
 
 impl Object {
-    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-        Object {x, y, char, color}
-    }
+    pub fn new(x: i32, y: i32, char: char, name: &str, color: Color, blocks: bool) -> Self {
+        Object {
+            x,
+            y,
+            char,
+            color,
+            name: name.into(),
+            blocks,
+            alive: false
 
-    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
-        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked
-        {
-            self.x += dx;
-            self.y += dy;
         }
     }
+
+
 
     pub fn draw(&self, con: &mut dyn Console) {
         con.set_default_foreground(self.color);
         con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    pub fn pos(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    pub fn set_pos(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
+    }
+}
+
+fn move_by(id: usize, dx: i32, dy: i32, map: &Map, objects: &mut [Object]) {
+    let (x, y) = objects[id].pos();
+    if !is_blocked(x + dx, y + dy, map, objects)
+    {
+        objects[id].set_pos(x+dx, y + dy);
     }
 }
 
@@ -138,7 +165,7 @@ struct Game {
     map: Map,
 }
 
-fn make_map(player: &mut Object) -> Map {
+fn make_map(objects: &mut Vec<Object>) -> Map {
     let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
     let mut rooms = vec![];
@@ -155,12 +182,12 @@ fn make_map(player: &mut Object) -> Map {
 
         if !failed {
             create_room(new_room, &mut map);
+            place_objects(new_room, &map, objects);
 
             let (new_x, new_y) = new_room.center();
 
             if rooms.is_empty() {
-                player.x = new_x;
-                player.y = new_y;
+                objects[PLAYER].set_pos(new_x, new_y);
             } else {
                 // all rooms after the first
                 // connects it to the previous room with a tunnel
@@ -203,16 +230,49 @@ fn create_v_tunnel(x: i32,y1: i32, y2: i32, map: &mut Map) {
     }
 }
 
+// Populating map
+
+fn place_objects(room: Rect, map: &Map ,objects: &mut Vec<Object>) {
+    let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS+1);
+
+    for _ in 0..num_monsters {
+        let x = rand::thread_rng().gen_range(room.x1, room.x2);
+        let y = rand::thread_rng().gen_range(room.y1, room.y2);
+
+        if !is_blocked(x, y, map, objects){
+            let mut monster = if rand::random::<f32>() < 0.8 {
+                Object::new(x, y, 'o', "orc", DESATURATED_GREEN, true)
+            } else {
+                Object::new(x, y, 'T', "troll", DARKER_GREEN, true)
+            };
+            monster.alive = true;
+            objects.push(monster);
+        }
+
+    }
+
+}
+
+fn is_blocked(x: i32, y: i32, map: &Map, objects: &[Object]) -> bool {
+    if map[x as usize][y as usize].blocked {
+        return true;
+    }
+
+    objects
+        .iter()
+        .any(|object| object.blocks && object.pos() == (x, y))
+}
+
 
 // HANDLE EVENTS
 
-fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
+fn handle_keys(tcod: &mut Tcod, game: &Game, objecs: &mut [Object]) -> bool {
     let key = tcod.root.wait_for_keypress(true);
     match key {
-        Key { code: Up, ..} => player.move_by(0, -1, game),
-        Key { code: Down, ..} => player.move_by(0, 1, game),
-        Key { code: Left, ..} => player.move_by(-1, 0, game),
-        Key { code: Right, ..} => player.move_by(1, 0, game),
+        Key { code: Up, ..} => move_by(PLAYER, 0, -1, &game.map, objecs),
+        Key { code: Down, ..} => move_by(PLAYER, 0, 1, &game.map, objecs),
+        Key { code: Left, ..} => move_by(PLAYER, -1, 0, &game.map, objecs),
+        Key { code: Right, ..} => move_by(PLAYER, 1, 0, &game.map, objecs),
         Key { code: Escape, ..} => {
             return true;
         }
@@ -237,14 +297,12 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(25,23, '@', WHITE);
+    let player = Object::new(25,23, '@', "player", WHITE, true);
 
-    let npc = Object::new(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
-
-    let mut objects = [player, npc];
+    let mut objects = vec![player];
 
     let mut game = Game {
-        map: make_map(&mut objects[0]),
+        map: make_map(&mut objects),
     };
 
     for y in 0..MAP_HEIGHT {
@@ -265,9 +323,9 @@ fn main() {
         render_all(&mut tcod, &mut game, &objects, fov_recompute);
 
         tcod.root.flush();
-        let player = &mut objects[0];
+        let player = &mut objects[PLAYER];
         previous_player_position = (player.x, player.y);
-        let exit = handle_keys(&mut tcod, &game, player);
+        let exit = handle_keys(&mut tcod, &game, &mut objects);
         if exit {
             break;
         }
@@ -278,7 +336,7 @@ fn render_all(mut tcod: &mut Tcod,game: &mut Game, objects: &[Object], fov_recom
     tcod.con.clear();
 
     if fov_recompute {
-        let player = &objects[0];
+        let player = &objects[PLAYER];
         tcod.fov
             .compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
     }
